efdc5c3e14933c6d22a7a95a35c39981aefd8711
G40N <g40n@G40Nui-MacBook-Air.local>
2017-02-13 15:10:05 +0900

    Edit wolgeup d-day as 24

--- wolgeup.py
+++ wolgeup.py
@@ -4,13 +4,13 @@ import jandiurl
 
 today = datetime.date.today()
 wolgeupnal = datetime.date(1, 1, 1)
-if today.day <= 26:
-    wolgeupnal = datetime.date(today.year, today.month, 26)
+if today.day <= 24:
+    wolgeupnal = datetime.date(today.year, today.month, 24)
 else:
     if today.month < 12:
-        wolgeupnal = datetime.date(today.year, today.month + 1, 26)
+        wolgeupnal = datetime.date(today.year, today.month + 1, 24)
     else:
-        wolgeupnal = datetime.date(today.year + 1, 1, 26)
+        wolgeupnal = datetime.date(today.year + 1, 1, 24)
 
 headers = {'Accept': 'application/vnd.tosslab.jandi-v2+json',
            'Content-Type': 'application/json; charset=utf-8'}

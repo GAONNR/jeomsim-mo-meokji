045f722b3919f38eb6053128cefd1a05cebe0c52
Ji Junseop <gaon0403@gmail.com>
2017-02-14 17:40:43 +0900

    Now jandi-bot give as three choices, also it probably doesn't work at Python2 anymore

--- jandi-bot.py
+++ jandi-bot.py
@@ -4,33 +4,44 @@ import time
 import jandiurl
 
 
-def choose_all_cases():
-    return (chooser.choose_my_lunch(0, False),
-            chooser.choose_my_lunch(1, False),
-            chooser.choose_my_lunch(2, False))
+def inside():
+    return chooser.choose_my_lunch(0, False)
 
-inside, outside, no_matter = choose_all_cases()
+
+def outside():
+    return chooser.choose_my_lunch(1, False)
+
+
+def no_matter():
+    return chooser.choose_my_lunch(2, False)
+
 now = time.localtime()
 
 headers = {'Accept': 'application/vnd.tosslab.jandi-v2+json',
            'Content-Type': 'application/json; charset=utf-8'}
+
+data_tuple = (now.tm_mon, now.tm_mday) + \
+    tuple(map(lambda x: outside(), range(3))) + \
+    tuple(map(lambda x: inside(), range(3))) + \
+    tuple(map(lambda x: no_matter(), range(3)))
+
 data = '''
 {
-    "body" : "☆★오늘 점심 뭐 먹지(%02d/%02d)★☆",
+    "body" : "오늘 점심 뭐 먹지(%02d/%02d) Update: 삼중택일 ver.",
     "connectColor" : "#2196f3",
     "connectInfo": [{
         "title" : "1. 나가먹",
-        "description" : "%s"
+        "description" : "%s, %s, %s"
     },
     {
         "title" : "2. 안에서먹",
-        "description" : "%s"
+        "description" : "%s, %s, %s"
     },
     {
         "title" : "3. 노상관",
-        "description" : "%s"
+        "description" : "%s, %s, %s"
     }]
 }
-''' % (now.tm_mon, now.tm_mday, outside, inside, no_matter)
+''' % data_tuple
 
 r = requests.post(jandiurl.url, data=data.encode('utf-8'), headers=headers)
